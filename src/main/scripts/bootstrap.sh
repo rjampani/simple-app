6ceee68798322ec1e86058ef02ce8a928d25a143
rjampani <jampany.ravindra@gmail.com>
2021-03-20 04:52:13 -0700

    bootstap ami script

---
+++ src/main/scripts/bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jre-headless
+sudo apt-get install sudo apt-get install python3-pi
+pip install awscli --upgrade --user
+export AWS_ACCESS_KEY_ID=AKIATOOROYR3MSDQEJEW
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
+sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomca
+wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.44/bin/apache-tomcat-9.0.44.tar.gz -P /tmp
+sudo tar xf /tmp/apache-tomcat-9*.tar.gz -C /opt/tomcat
+sudo ln -s /opt/tomcat/apache-tomcat-9.0.44 /opt/tomcat/latest
+sudo chown -RH tomcat: /opt/tomcat/latest
+sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+sudo cat << EOF > /tmp/tomcat.service
+[Unit]
+Description=Tomcat 9 servlet container
+After=network.target
+
+[Service]
+Type=forking
+
+User=tomcat
+Group=tomcat
+
+Environment="JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
+Environment="JAVA_OPTS=-Djava.security.egd=file:///dev/urandom -Djava.awt.headless=true"
+
+Environment="CATALINA_BASE=/opt/tomcat/latest"
+Environment="CATALINA_HOME=/opt/tomcat/latest"
+Environment="CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+
+ExecStart=/opt/tomcat/latest/bin/startup.sh
+ExecStop=/opt/tomcat/latest/bin/shutdown.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv /tmp/tomcat.service /etc/systemd/system/tomcat.service
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl enable tomcat
+aws s3 cp s3://simple-app-artifact-store/simple-app.war /tmp
+mv /tmp/simple-app.war /opt/tomcat/latest/webapps
+sudo systemctl restart tomcat
